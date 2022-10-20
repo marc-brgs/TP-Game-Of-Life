e6aa4c1c3d8aa57dff8f24b501daf4073d39e4cb
Marc Brgs <marc.brgs@yahoo.fr>
2022-10-20 14:41:47 +0200

    PopulationControl buggy super fast

---
+++ GameOfLife3D/Assets/Scripts/Cell.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    public int voisins = 0;
+    public bool alive = false;
+
+    /*public Cell(int x, int y)
+    {
+        obj = new GameObject();
+        var mf = obj.AddComponent<MeshFilter>();
+        var mr = obj.AddComponent<MeshRenderer>();
+        mf.mesh = Resources.GetBuiltinResource<Mesh>("Cube.fbx");
+        
+        obj.transform.position = new Vector3(x, y, 0f);
+        voisins = 0;
+        alive = false;
+    }*/
+    
+    public void setAlive(bool alive)
+    {
+        this.alive = alive;
+        
+        if (alive)
+            GetComponent<MeshRenderer>().enabled = true;
+        else
+            GetComponent<MeshRenderer>().enabled = false;
+    }
+
+        // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- GameOfLife3D/Assets/Scripts/GameManager.cs
+++ GameOfLife3D/Assets/Scripts/GameManager.cs
@@ -1,71 +1,143 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public class Case
-    {
-        public GameObject obj;
-        public int voisins;
-        public bool alive;
+    int SCREEN_X = 86;
+    int SCREEN_Y = 48;
 
-        public Case(int x, int y)
-        {
-            obj = new GameObject();
-            var mf = obj.AddComponent<MeshFilter>();
-            var mr = obj.AddComponent<MeshRenderer>();
-            mf.mesh = Resources.GetBuiltinResource<Mesh>("Cube.fbx");
-            
-            obj.transform.position = new Vector3(x, y, 0f);
-            voisins = 0;
-            alive = false;
-        }
-        
-        public void setAlive()
-        {
-            if (alive)
-                obj.GetComponent<MeshRenderer>().enabled = true;
-            else
-                obj.GetComponent<MeshRenderer>().enabled = false;
-        }
-    }
-
-    [SerializeField] private Case[][] board;
+    [SerializeField] private Cell[][] board;
     
     // Start is called before the first frame update
     void Start()
     {
-        int x = 86;
-        int y = 48;
-        
-        board = new Case[x][];
+        board = new Cell[SCREEN_X][];
 
-        for (int i = 0; i < x; i++)
+        for (int i = 0; i < SCREEN_X; i++)
         {
-            board[i] = new Case[y];
+            board[i] = new Cell[SCREEN_Y];
         }
 
-        for (int i = 0; i < x; i++)
+        for (int i = 0; i < SCREEN_X; i++)
         {
-            for (int j = 0; j < y; j++)
+            for (int j = 0; j < SCREEN_Y; j++)
             {
-                board[i][j] = new Case(i, j);
+                // board[i][j] = new Cell(i, j);
+                board[i][j] = Instantiate(Resources.Load("Cube", typeof(Cell)), new Vector3(i, j, 0f), Quaternion.identity) as Cell;
+                board[i][j].setAlive(randomActiveCell());
             }
         }
-        
-        Debug.Log(board[0][2].alive);
     }
 
     // Update is called once per frame
     void Update()
     {
+        CountNeighbors();
         
+        populationControl();
     }
 
-    void renderBoard()
+    void CountNeighbors()
     {
-        
+        for (int x = 0; x < SCREEN_X; x++)
+        {
+            for (int y = 0; y < SCREEN_Y; y++)
+            {
+                // N
+                int numNeighbors = 0;
+                if (y + 1 < SCREEN_Y)
+                {
+                    if (board[x][y + 1].alive)
+                        numNeighbors++;
+                }
+                
+                // E
+                if (x + 1 < SCREEN_X)
+                {
+                    if (board[x + 1][y].alive)
+                        numNeighbors++;
+                }
+                
+                // S
+                if (y - 1 >= 0)
+                {
+                    if (board[x][y - 1].alive)
+                        numNeighbors++;
+                }
+                
+                // W
+                if (x - 1 >= 0)
+                {
+                    if (board[x - 1][y].alive)
+                        numNeighbors++;
+                }
+                
+                // NE
+                if (x + 1 < SCREEN_X && y + 1 < SCREEN_Y)
+                {
+                    if (board[x + 1][y + 1].alive)
+                        numNeighbors++;
+                }
+                
+                // NW
+                if (x - 1 >= 0 && y + 1 < SCREEN_Y)
+                {
+                    if (board[x-1][y+1].alive)
+                        numNeighbors++;
+                }
+                
+                // SE
+                if (x + 1 < SCREEN_Y && y - 1 >= 0)
+                {
+                    if (board[x+1][y-1].alive)
+                        numNeighbors++;
+                }
+                
+                // SW
+                if (x - 1 >= 0 && y - 1 >= 0)
+                {
+                    if (board[x-1][y-1].alive)
+                        numNeighbors++;
+                }
+                
+                board[x][y].voisins = numNeighbors;
+            }
+        }
+       
+    }
+
+    bool randomActiveCell()
+    {
+        int rand = UnityEngine.Random.Range(0, 100);
+        if (rand > 80)
+            return true;
+        return false;
+    }
+
+    void populationControl()
+    {
+        for (int x = 0; x < SCREEN_X; x++)
+        {
+            for (int y = 0; y < SCREEN_Y; y++)
+            {
+                if (board[x][y].alive)
+                {
+                    if (board[x][y].voisins != 2 || board[x][y].voisins != 3)
+                    {
+                        board[x][y].setAlive(false);
+                    }
+                }
+                else
+                {
+                    if (board[x][y].voisins == 3)
+                    {
+                        board[x][y].setAlive(true);
+                    }
+                }
+            } 
+        }
     }
 }
