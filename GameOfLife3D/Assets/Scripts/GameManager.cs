b8f5dc33355293acc555b2ffd968cf8af18d02b9
Marc Brgs <marc.brgs@yahoo.fr>
2022-10-20 11:21:09 +0200

    Unity setup and GameManager
    - Unity 2021.3.10f1
    - Added .gitignore

---
+++ GameOfLife3D/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public class Case
+    {
+        public GameObject obj;
+        public int voisins;
+        public bool type;
+    }
+
+    private Case[][] board;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
